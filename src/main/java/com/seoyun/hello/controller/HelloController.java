16a9135102607aa2f15ea25ff3c51b9dfa02be8f
seoyun <newseoyun@gmail.com>
2020-09-01 00:36:49 +0900

    chapter9

--- src/main/java/com/seoyun/hello/controller/HelloController.java
+++ src/main/java/com/seoyun/hello/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.seoyun.hello.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,40 @@ public class HelloController {
         model.addAttribute("data", "hello!!");
         return "hello";
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) { /* ctrl P 누르면 매소드 옵션정보 */
+        model.addAttribute("name", name);
+        return "hello-template";  /* ctrl shift enter => ;붙여줌 */
+    }
+
+
+    /* @ResponseBody : HttpMessageConverter가 JSON Converter(객체) vs String Converter(문자) 중 실행 */
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name; /* String 그대로 반환 */
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; /* JSON(키-값) 객체로 반환 */
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
